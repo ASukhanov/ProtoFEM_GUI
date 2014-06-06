5862d9f9b15cb7ea3906879b8716268019c8fb60
ASukhanov <sukhanov@bnl.gov>
2014-06-06 17:10:32 -0400

    v18. Data directory name and serial port name ofr linux are moved to config file. This is to eliminate changes in main code.

---
+++ config.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+#Version 4
+
+class Config():
+
+	# non-Windows serial device name
+	#serial_device  =	'/dev/tty*'	# MAC
+	#serial_device  =	'/dev/ttyUSB*'	# USB to serial devices
+        serial_device  =	'/dev/ttyAMA*'	# RPI on-board serial port
+
+	# data directory
+	# data_directory =	'D:/data/'
+	data_directory =	'../../data/'
+
